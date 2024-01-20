c2ec8425940470b20c877997d60e6532cd993737
yvens94 <ayvenjee@gmail.com>
2024-01-20 00:09:11 -0500

    PORT8080

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def predict_datapoint():
         return render_template("home.html", results=results[0])
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000)  
+    app.run(host="0.0.0.0", port=8080)  
 
 
 
